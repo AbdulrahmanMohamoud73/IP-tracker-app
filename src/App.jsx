65fa7b90466d54fe0766b180b7c6a761694526d3
mojr <mojr1717@gmail.com>
2023-06-28 19:29:05 +0100

    set up fields to display API response results

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import backgroundimg from '../images/pattern-bg-desktop.png'
 import arrow from '../images/icon-arrow.svg'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
 
 function App() {
 
-  async function getlocation (){
-    await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.API_KEY}&ipAddress=8.8.8.8`).then(response => console.log(response)).catch(err => console.log(err))
+  const [inputVal, SetinputVal] = useState('');
+  const [responsedata, Setresponsedata] = useState(null);
+
+  const getlocation = async() => {
+    await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_API_KEY}&ipAddress=${inputVal}`).then(response => response.json()).then(data => {Setresponsedata(data); console.log(data)}).catch(err => console.log(err))
   }
 
   return (
@@ -18,27 +22,27 @@ function App() {
     <form className='relative h-1/3 font-rubik text-xs flex flex-col gap-5 items-center justify-center'>
       <label className='text-white font-rubik font-medium text-3xl'>IP Address Tracker</label>
       <div className='flex justify-center w-full'>
-        <input type="text" placeholder="Search any IP address or domain" className="rounded-l-md outline-none text-base p-6 h-12 w-2/6"/>
-        <button><img src={arrow} className='bg-black h-12 p-4 rounded-r-md'/></button>
+        <input type="text" onChange={ e => SetinputVal(e.target.value)} placeholder="Search any IP address or domain" className="rounded-l-md outline-none text-base p-6 h-12 w-2/6"/>
+        <button><img  onClick={ e => { e.preventDefault(); getlocation()}} src={arrow} className='bg-black h-12 p-4 rounded-r-md'/></button>
       </div>
     </form>
     <div className="absolute w-screen flex justify-center items-center">
-      <div className='w-3/5 h-m-5 -top-10 shadow-lg lg:grid grid-cols-4 sm:grid-cols-1 sm:grid-rows-4 content-center font-rubik border-black border-2 rounded-3xl bg-white relative z-50'>
+      <div className='w-3/5 h-40 -top-10 shadow-lg lg:grid grid-cols-4 sm:grid-cols-1 sm:grid-rows-4 content-center font-rubik border-black border-2 rounded-3xl bg-white relative z-50'>
         <div className="h-fit border-r-slate-300 border-r-2">
         <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>IP ADDRESS</h2>
-        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>192.212.174</h1>
+        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>{responsedata?.ip}</h1>
         </div>
         <div className="h-fit border-r-slate-300 border-r-2">
-        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>IP ADDRESS</h2>
-        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>192.212.174</h1>
+        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>ISP</h2>
+        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>{responsedata?.isp}</h1>
         </div>
         <div className="h-fit border-r-slate-300 border-r-2">
-        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>IP ADDRESS</h2>
-        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>192.212.174</h1>
+        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>Location</h2>
+        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>{`${responsedata?.location?.region}, ${responsedata?.location?.country}`}</h1>
         </div>
         <div className="h-1/2">
-        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>IP ADDRESS</h2>
-        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>192.212.174</h1>
+        <h2 className='font-bold text-sm uppercase text-slate-500 tracking-wide mx-5'>TIMEZONE</h2>
+        <h1 className='font-bold text-3xl uppercase text-black tracking-wide mx-4 my-3'>{`UTC ${responsedata?.location?.timezone}`}</h1>
         </div>
       </div>
     </div>
